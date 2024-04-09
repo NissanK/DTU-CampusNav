4a6687f986bf0247f415d526d98d5356b55eb740
Nissan Kumar <nissankumar9001@gmail.com>
2024-04-09 19:12:20 +0530

    separated backend
    folder structure

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -4,7 +4,7 @@ function MainPageDiv() {
     return (
         <div className='flex mt-12 justify-center items-center flex-col px-3 py-3 md:py-4
         bg-dark-blue opacity-80 text-center text-xs md:text-base'>
-            © Copyright DTU CampusNav 2023. All Rights Reserved Designed with ❤️ and 🖥️
+            © Copyright DTU CampusNav 2023. All Rights Reserved, designed with ❤️ and 🖥️
         </div>
     )
   }
