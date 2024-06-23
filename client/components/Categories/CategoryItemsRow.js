b50aed7b56b82fc66029db3736b0128fbbda0520
Nissan Kumar <nissankumar9001@gmail.com>
2024-06-23 05:34:37 +0530

    Updated the ordering of category items to original

--- client/components/Categories/CategoryItemsRow.js
+++ client/components/Categories/CategoryItemsRow.js
@@ -3,11 +3,16 @@ import CategoryIcons from './CategoryIcons'
 import categoryItemsData from './CategoryItemsData'
 
 function CategoryItemsRow() {
+
+  const orderedKeys = ['57240116', '15699078', '88618413', '62637776', '66350266', '23773950', '13418771', '46172799'];
+
+  const renderCategoryIcons = orderedKeys.map((id) => (
+      <CategoryIcons category={categoryItemsData[id]} id={id} key={id}></CategoryIcons>
+  ));
+  
   return (
     <div className='flex items-center justify-around mx-2 lg:mx-5 my-4 md:my-7 lg:my-10 flex-wrap'>
-      {Object.keys(categoryItemsData).map((id) => (
-          <CategoryIcons category={categoryItemsData[id]} id={id} key={id}></CategoryIcons>
-      ))}
+      {renderCategoryIcons}
     </div>
   )
 }
